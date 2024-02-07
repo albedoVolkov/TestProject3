d8a3dfd85a1a1380a0a54d3323b5b71e921e081d
albedoVolkov <120480335+albedoVolkov@users.noreply.github.com>
2024-02-07 23:47:57 +0700

    commit 1 - first commit

---
+++ app/src/main/java/com/albedo/testproject3/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.albedo.testproject3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import java.nio.file.Paths.get
+
+class MainActivity : AppCompatActivity() {
+//    1. Выводится краткая информация о пользователях (ФИО, фотография, адрес, номер
+//    телефона) в виде списка, полученная от https://randomuser.me ;
+//    2. По клику на элемент списка на отдельном экране показывается полная информация о
+//    выбранном пользователе;
+//    3. Данные о пользователях не теряются при перезапуске приложения;
+//    4. Нажатие на Email, номер телефона, адрес/координаты отправляет пользователя в
+//    приложение, которое может обработать эти данные (почта, звонилка, карты);
+//    5. Список пользователей можно обновить принудительно;
+//    6. Пользователю выводятся уведомления о возникших ошибках при загрузке данных или
+//    работе с ними.
+
+//Data
+// https://randomuser.me/api/?results=15
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
